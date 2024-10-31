ff271be9f20263d1998cd4db67a431fd4585a04f
alesyaisachenko <alesyaisachenko87@gmail.com>
2024-10-31 21:48:51 +0300

    add Constanys class

---
+++ src/main/java/org/example/Constants.java
@@ -0,0 +1,6 @@
+package org.example;
+
+public class Constants {
+    public static String MAIN_PAGE_URL = "https://qa-scooter.praktikum-services.ru/";
+    public static String ORDER_PAGE_URL = MAIN_PAGE_URL + "order";
+}
